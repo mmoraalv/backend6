780f7bafd3092a0c51146bed86a0c97fa977e5c7
mariomora <m.mora.alv@gmail.com>
2023-10-04 01:36:31 -0600

    Se implementa autenticación de Github

--- src/config/passport.js
+++ src/config/passport.js
@@ -1,7 +1,9 @@
 import local from 'passport-local' //Estrategia
 import passport from 'passport' //Manejador de las estrategias
+import GithubStrategy from 'passport-github2'
 import { createHash, validatePassword } from '../utils/bcrypt.js'
 import { userModel } from '../models/users.model.js'
+
 
 //Defino la estrategia a utilizar
 const LocalStrategy = local.Strategy
@@ -14,7 +16,7 @@ const initializePassport = () => {
             const { first_name, last_name, email, age } = req.body
 
             try {
-                const user = await userModel.findOne({ email: username })
+                const user = await userModel.findOne({ email: email })
                 if (user) {
                     //done es como si fuera un return de un callback
                     return done(null, false)
@@ -36,17 +38,6 @@ const initializePassport = () => {
         }
     ))
 
-     //Inicializar la session del usuario
-     passport.serializeUser((user, done) => {
-        done(null, user._id)
-    })
-
-    //Eliminar la session del usuario
-    passport.deserializeUser(async (id, done) => {
-        const user = await userModel.findById(id)
-        done(null, user)
-    })
-
     passport.use('login', new LocalStrategy({ usernameField: 'email' }, async (username, password, done) => {
         try {
             const user = await userModel.findOne({ email: username })
@@ -66,6 +57,50 @@ const initializePassport = () => {
         }
 
     }))
+
+    passport.use('github', new GithubStrategy({
+        clientID: process.env.CLIENT_ID,
+        clientSecret: process.env.CLIENT_SECRET,
+        callbackURL: process.env.CALLBACK_URL
+
+    }, async (accessToken, refreshToken, profile, done) => {
+
+        try {
+            console.log(accessToken)
+            console.log(refreshToken)
+            console.log(process.env.CALLBACK_URL)
+            const user = await userModel.findOne({ email: profile._json.email })
+            if (!user) {
+                const userCreated = await userModel.create({
+                    first_name: profile._json.name,
+                    last_name: ' ',
+                    email: profile._json.email,
+                    age: 18, //Edad por defecto,
+                    password: 'password'
+                })
+                done(null, userCreated)
+
+            } else {
+                done(null, user)
+            }
+
+        } catch (error) {
+            done(error)
+        }
+
+    }))
+
+    //Inicializar la session del usr
+    passport.serializeUser((user, done) => {
+        done(null, user._id)
+    })
+
+    //Eliminar la session del usr
+    passport.deserializeUser(async (id, done) => {
+        const user = await userModel.findById(id)
+        done(null, user)
+    })
+
 }
 
 export default initializePassport
